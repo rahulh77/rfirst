db13cc51ca3ba9c2099c387ba5afd77ae49751ee
Rahul <rahul777@outlook.com>
2017-12-26 03:55:46 -0500

    State and props example, Person component

---
+++ src/Person/Person.js
@@ -0,0 +1,29 @@
+//Naming convention for a component is firstCharacter in Caps
+// function names starts with smaller case
+import React from "react";
+
+//using es6 function syntax to create a component (Best practice)
+const person = props => {
+  return (
+    <div style={{ background: "pink" }}>
+      <p>
+        I am {props.name} and {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+      {/* <p>Hello from Person Component </p>
+      <p>Component is a function that returns JSX</p> */}
+    </div>
+  );
+};
+
+export default person;
+
+// class Person extends Component {
+//     render() {
+//         return (
+//             <div>
+
+//             </div>
+//         );
+//     }
+// }
